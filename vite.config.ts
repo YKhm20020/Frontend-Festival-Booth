c61fd3b70544be2c5bc319d0178443504c2a1bca
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-09 00:52:28 +0900

    fix: package-lock,json, node_modules削除後にstorybookのdocsが表示されない問題を解消

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    force: true,
+	exclude: ['node_modules/.cache/storybook']
+  },
 })
