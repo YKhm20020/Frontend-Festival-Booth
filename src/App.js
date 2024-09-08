748b3e87bfdd02e188a95c1bce5f301197fd45da
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-08 23:01:18 +0900

    update: 最初にtailwindをインストール、biome, storybookは後で入れ直す

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around items-center">
+      <h1 className="text-3xl font-bold underline">
+        Hello Tailwind CSS!
+      </h1>
+      <p className="m-0 text-gray-400">Tailwind CSSです</p>
+      <button className="bg-gray-300 border-0 p-2 rounded-md hover:bg-gray-400 hover:text-white">ボタン</button>
     </div>
   );
 }
