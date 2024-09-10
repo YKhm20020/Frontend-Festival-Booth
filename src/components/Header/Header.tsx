295e74d7d65423c551c571341e78a85e8934e667
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-10 22:28:33 +0900

    add: メインページに配置するヘッダーを仮作成

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,15 @@
 import type React from 'react';
-import { Link } from '@tanstack/react-router';
+import { NavLink } from './_components/NavLink';
 
 export const Header: React.FC = () => {
 	return (
-		<header className='bg-black'>
-			<ul className=''>
-				<Link to='/' className=''>
-					Home
-				</Link>
-			</ul>
-			<ul className=''>
-				<Link to='/write' className=''>
-					Write
-				</Link>
-			</ul>
+		<header className='bg-blue-400 flex flex-col w-screen h-10'>
+			<div className='container mx-auto my-auto flex flex-wrap flex-col md:flex-row items-center'>
+				<nav className='ml-auto flex flex-wrap items-center justify-center '>
+					<NavLink toLink='/' toName='Home' />
+					<NavLink toLink='/' toName='Write' />
+				</nav>
+			</div>
 		</header>
 	);
 };
