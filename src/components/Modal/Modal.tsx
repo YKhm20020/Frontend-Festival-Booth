6ff7f0d9070d6bdc7eef8513c1f33f66cf20ed4d
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-12 02:16:44 +0900

    refactor: 消せていなかった条件分岐を削除

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -44,10 +44,6 @@ export const Modal: React.FC<ModalProps> = ({
 		return null;
 	}
 
-	if (!Array.isArray(links)) {
-		return null;
-	}
-
 	return (
 		<div className='fixed inset-0 z-50 flex items-center justify-center'>
 			<div className='fixed inset-0 bg-black bg-opacity-50' onClick={closeModal}></div>
