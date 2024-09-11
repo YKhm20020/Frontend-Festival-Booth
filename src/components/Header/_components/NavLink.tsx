8705e79673e4611fc1df3fdf519b4646494b3ef9
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-11 17:05:12 +0900

    update: ナビゲーションリンクとして表示する名前をpropsではなくchildrenで管理するよう変更

--- src/components/Header/_components/NavLink.tsx
+++ src/components/Header/_components/NavLink.tsx
@@ -1,15 +1,21 @@
-import type React from 'react';
-import { Link } from '@tanstack/react-router';
+import type React from "react";
+import { Link } from "@tanstack/react-router";
 
 type NavLinkProps = {
 	toLink: string;
-	toName: string;
+	children: React.ReactNode;
 };
 
-export const NavLink: React.FC<NavLinkProps> = ({ toLink, toName }) => {
+export const NavLink: React.FC<NavLinkProps> = ({ toLink, children }) => {
 	return (
-		<Link to={toLink} className='mr-5 text-gray-600 hover:text-gray-900'>
-			{toName}
+		<Link
+			to={toLink}
+			className="mr-5 text-gray-600 relative cursor-pointer
+            after:absolute after:bg-gray-900 after:h-0.5 after:w-0 after:bottom-0 after:left-0 after:transition-all after:duration-200
+            hover:text-gray-900 hover:underline-offset-2
+            hover:after:w-full"
+		>
+			{children}
 		</Link>
 	);
 };
