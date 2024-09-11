56575b3eedae75137ceeb34fb6c6055ddcb380e5
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-12 05:07:34 +0900

    update: レスポンシブデザインを考慮するため、カードの幅などを調整

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -31,34 +31,32 @@ export const Card: React.FC<CardProps> = ({
 
 	return (
 		<>
-			<div className='flex justify-center w-full'>
-				<div
-					className='relative group flex flex-col shadow-2xl rounded-xl overflow-hidden w-60 md:w-72 lg:w-80 cursor-pointer transition-all duration-300 ease-in-out
+			<div
+				className='relative group flex flex-col shadow-2xl rounded-xl overflow-hidden w-40 md:w-48 lg:w-56 cursor-pointer transition-all duration-300 ease-in-out
                     hover:scale-105'
-					onClick={openModal}
-				>
-					<img
-						className='object-cover object-center w-60 h-60 md:w-72 lg:w-80'
-						src={src}
-						alt={alt ?? title}
-						width={300}
-						height={300}
-					/>
-					<div className='inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300' />
-					<div className='absolute bg-white inset-x-0 bottom-0 p-4 transform translate-y-[calc(100%-4.5rem)] group-hover:translate-y-0 transition-transform duration-500 ease-in-out'>
-						<h1 className='text-2xl font-semibold text-neutral-800 mb-4 truncate py-1'>
-							{title}
-						</h1>
-						<div className='space-y-1 overflow-hidden'>
-							{links.map((item, index) => (
-								<p
-									key={index}
-									className='text-base text-transparent group-hover:text-gray-600 truncate transition-colors duration-500 delay-200 ease-in-out'
-								>
-									{item}
-								</p>
-							))}
-						</div>
+				onClick={openModal}
+			>
+				<img
+					className='object-cover object-center w-40 h-60 md:w-72 lg:w-80'
+					src={src}
+					alt={alt ?? title}
+					width={300}
+					height={300}
+				/>
+				<div className='inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300' />
+				<div className='absolute bg-white inset-x-0 bottom-0 p-4 transform translate-y-[calc(100%-4.5rem)] group-hover:translate-y-0 transition-transform duration-500 ease-in-out'>
+					<h1 className='text-2xl font-semibold text-neutral-800 mb-4 truncate py-1'>
+						{title}
+					</h1>
+					<div className='space-y-1 overflow-hidden'>
+						{links.map((item, index) => (
+							<p
+								key={index}
+								className='text-base text-transparent group-hover:text-gray-600 truncate transition-colors duration-500 delay-200 ease-in-out'
+							>
+								{item}
+							</p>
+						))}
 					</div>
 				</div>
 			</div>
