b3e2a9f1d522de7823887eb6fcd8b85bf70b0c4a
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-12 05:31:28 +0900

    update: レスポンシブデザインを考慮し、smで列が半分、行が2倍になるよう変更

--- src/pages/IntroductionListPage.tsx
+++ src/pages/IntroductionListPage.tsx
@@ -9,7 +9,7 @@ export const IntroductionListPage: React.FC = () => {
 			<div className='w-full max-w-screen-lg mx-auto p-4 my-8'>
 				<div className='relative'>
 					{/* カードのグリッド */}
-					<div className='grid grid-cols-4 grid-rows-2 gap-x-6 gap-y-8 place-items-center justify-center items-center'>
+					<div className='grid grid-cols-2 md:grid-cols-4 md:grid-rows-2 lg:gap-x-6 gap-y-8 place-items-center justify-center items-center'>
 						{/* カードコンポーネント（8個） */}
 						{[...Array(8)].map((_, index) => (
 							<Card
@@ -28,10 +28,10 @@ export const IntroductionListPage: React.FC = () => {
 					<div className='absolute left-0 lg:-left-12 top-1/2 -translate-y-1/2'>
 						<button
 							id='slider-button-left'
-							className='group p-2 flex justify-center items-center border border-solid border-indigo-600 w-10 h-10 md:w-12 md:h-12 transition-all duration-500 rounded-full hover:bg-indigo-600 bg-white bg-opacity-50'
+							className='group p-2 flex justify-center items-center border border-solid border-indigo-600 w-20 h-20 md:w-16 md:h-16 transition-all duration-500 rounded-full hover:bg-indigo-600 bg-white bg-opacity-50'
 						>
 							<svg
-								className='h-4 w-4 md:h-5 md:w-5 text-indigo-600 group-hover:text-white'
+								className='h-10 w-10 md:h-6 md:w-6 text-indigo-600 group-hover:text-white'
 								xmlns='http://www.w3.org/2000/svg'
 								viewBox='0 0 16 16'
 								fill='none'
@@ -49,10 +49,10 @@ export const IntroductionListPage: React.FC = () => {
 					<div className='absolute right-0 lg:-right-12 top-1/2 -translate-y-1/2'>
 						<button
 							id='slider-button-right'
-							className='group p-2 flex justify-center items-center border border-solid border-indigo-600 w-10 h-10 md:w-12 md:h-12 transition-all duration-500 rounded-full hover:bg-indigo-600 bg-white bg-opacity-50'
+							className='group p-2 flex justify-center items-center border border-solid border-indigo-600 w-20 h-20 md:w-16 md:h-16 transition-all duration-500 rounded-full hover:bg-indigo-600 bg-white bg-opacity-50'
 						>
 							<svg
-								className='h-4 w-4 md:h-5 md:w-5 text-indigo-600 group-hover:text-white'
+								className='h-10 w-10 md:h-6 md:w-6 text-indigo-600 group-hover:text-white'
 								xmlns='http://www.w3.org/2000/svg'
 								viewBox='0 0 16 16'
 								fill='none'
