183de44badcdee198e4461b1924663e5d326dc53
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-13 05:57:19 +0900

    update: 現在のページにいる場合は、ホバー時のCSSを適用する機能を追加

--- src/components/Header/_components/NavLink.tsx
+++ src/components/Header/_components/NavLink.tsx
@@ -1,5 +1,5 @@
-import type React from "react";
-import { Link } from "@tanstack/react-router";
+import type React from 'react';
+import { Link, useLocation } from '@tanstack/react-router';
 
 type NavLinkProps = {
 	toLink: string;
@@ -7,13 +7,17 @@ type NavLinkProps = {
 };
 
 export const NavLink: React.FC<NavLinkProps> = ({ toLink, children }) => {
+	const location = useLocation();
+	const isActive = location.pathname === toLink;
+
 	return (
 		<Link
 			to={toLink}
-			className="mr-5 text-gray-600 relative cursor-pointer
+			className={`mr-5 text-gray-600 relative cursor-pointer
             after:absolute after:bg-gray-900 after:h-0.5 after:w-0 after:bottom-0 after:left-0 after:transition-all after:duration-200
             hover:text-gray-900 hover:underline-offset-2
-            hover:after:w-full"
+            hover:after:w-full
+            ${isActive ? 'text-gray-900 font-bold after:w-full' : ''}`}
 		>
 			{children}
 		</Link>
