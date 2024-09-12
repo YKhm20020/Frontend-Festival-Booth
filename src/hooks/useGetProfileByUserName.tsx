5e2998072e68b04df84f5abd07c606d50b95570d
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-13 01:59:42 +0900

    update: 扱う情報は同じであるため、name と username を username と統一

--- src/hooks/useGetProfileByUserName.tsx
+++ src/hooks/useGetProfileByUserName.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-type UseGetProfileByNameProps = {
+type UseGetProfileByUserNameProps = {
 	name: string; // ユーザー名
 };
 
-export const useGetProfileByName = ({ name }: UseGetProfileByNameProps) => {
+export const useGetProfileByName = ({ name }: UseGetProfileByUserNameProps) => {
 	const [data, setData] = useState<any>(null);
 	const [loading, setLoading] = useState<boolean>(true);
 	const [error, setError] = useState<string | null>(null);
@@ -13,11 +13,23 @@ export const useGetProfileByName = ({ name }: UseGetProfileByNameProps) => {
 	useEffect(() => {
 		const fetchProfile = async () => {
 			setLoading(true);
+			setError(null);
+
 			try {
+				// リクエストしたけど2xxの範囲外
 				const response = await axios.get(`/profiles/${name}`);
 				setData(response.data);
 			} catch (err) {
-				setError(err.message);
+				if (err.response) {
+					// リクエストしたけど2xxの範囲外
+					setError(err.response.data?.message || 'Failed to fetch profile');
+				} else if (err.request) {
+					// リクエストしたけど応答がない
+					setError('No response from server.');
+				} else {
+					// その他のエラー
+					setError('Error: ', err.message);
+				}
 			} finally {
 				setLoading(false);
 			}
