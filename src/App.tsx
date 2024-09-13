b68b7b61711d751d3723768d6eb589e730547589
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-14 01:48:48 +0900

    fix: コンポーネントとして再利用可能とするため、ボタン内のテキストをchildrenとして持たせるよう修正

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 				<h1 className='text-3xl font-bold underline'>Hello world!</h1>
 				<div className='my-4' />
 				<div className='flex items-center justify-center grid grid-rows-1 grid-flow-col gap-8'>
-					<ToIntroductionPageButton />
+					<ToIntroductionPageButton>自己紹介を見る</ToIntroductionPageButton>
 					<ToProductsPageButton />
 				</div>
 				<hr className='flex-1 border-4 border-gray-500 my-8'></hr>
