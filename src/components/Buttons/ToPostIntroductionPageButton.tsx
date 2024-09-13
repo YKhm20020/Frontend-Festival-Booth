13b61565bec2fb40f7d7a2746f71da8d2bb2f868
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-14 01:53:43 +0900

    update: ボタンをコンポーネントとして再利用可能とするため、成果物投稿画面へ遷移するボタンのテキストをchildrenとしてもたせるよう修正

--- src/components/Buttons/ToPostIntroductionPageButton.tsx
+++ src/components/Buttons/ToPostIntroductionPageButton.tsx
@@ -1,16 +1,22 @@
+import type React from 'react';
 import { useNavigate } from '@tanstack/react-router';
 
-export const ToPostIntroductionPageButton = () => {
+type ToPostIntroductionPageButtonProps = {
+	children: React.ReactNode;
+};
+
+export const ToPostIntroductionPageButton: React.FC<ToPostIntroductionPageButtonProps> = ({
+	children,
+}: ToPostIntroductionPageButtonProps) => {
 	const navigate = useNavigate();
 
 	return (
 		<button
-			href='#_'
 			className='w-72 h-44 text-3xl relative rounded px-5 py-2.5 overflow-hidden group bg-fuchsia-800 hover:bg-gradient-to-r hover:from-white hover:to-white text-white hover:text-fuchsia-800 hover:ring-4 hover:ring-offset-2 hover:ring-fuchsia-800 transition-all ease-out duration-300'
-			onClick={() => navigate({to: '/write-introduction'})}
+			onClick={() => navigate({ to: '/write-introduction' })}
 		>
 			<span className='absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-fuchsia-950 opacity-10 rotate-12 group-hover:-translate-x-40 ease' />
-			<span className='relative'>自己紹介する</span>
+			<span className='relative'>{children}</span>
 		</button>
 	);
 };
