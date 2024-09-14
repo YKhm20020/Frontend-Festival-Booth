f05c1f77b6ed4830e4e0b9bca110320170ad6c83
KakinokiKanta <kanta12621@gmail.com>
2024-09-14 22:18:10 +0900

    add: storybook用のファイルを追加

---
+++ src/pages/Matching/QuestionMatchingPage.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { QuestionMatchingPage } from "./QuestionMatchingPage";
+
+const meta = {
+    title: 'FestivalBooth/Pages/Matching/QuestionMatchingPage',
+    component: QuestionMatchingPage,
+} satisfies Meta<typeof QuestionMatchingPage>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Default: Story = {};
