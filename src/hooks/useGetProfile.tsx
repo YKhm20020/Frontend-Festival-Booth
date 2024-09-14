945b58e2861790309feeffe58950ae61454a66f0
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-15 02:46:13 +0900

    update: console.logで環境変数をチェック

--- src/hooks/useGetProfile.tsx
+++ src/hooks/useGetProfile.tsx
@@ -19,6 +19,8 @@ export const useGetProfile = ({ page, limit }: UseGetProfileProps) => {
 	const [loading, setLoading] = useState<boolean>(true);
 	const [error, setError] = useState<string | null>(null);
 
+	console.log(`VITE_APP_BASE_URL: ${import.meta.env.VITE_APP_BASE_URL}`);
+
 	useEffect(() => {
 		const fetchData = async () => {
 			setLoading(true);
