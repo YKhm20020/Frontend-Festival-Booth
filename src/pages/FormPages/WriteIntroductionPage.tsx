279a9693cae0d3c8ed8d6bcbcaf8ce950870fae1
takahashi <tkhs.tmhr39@gmail.com>
2024-09-14 20:02:04 +0900

    fix: success状態でのみ画面を表示するように修正

--- src/pages/FormPages/WriteIntroductionPage.tsx
+++ src/pages/FormPages/WriteIntroductionPage.tsx
@@ -24,7 +24,7 @@ export const WriteIntroductionPage: React.FC = () => {
 
 	// successがfalseのときにauthページにリダイレクトする処理を追加
 	useEffect(() => {
-		if (!loading && success === false) {
+		if (!loading && !success) {
 			navigate({ to: '/auth' });
 		}
 	}, [loading, success, navigate]);
@@ -60,163 +60,164 @@ export const WriteIntroductionPage: React.FC = () => {
 
 				{/* ローディング中の処理 */}
 				{loading && <p>Loading...</p>}
-
-				<div className='w-full max-w-xl'>
-					<h1 className='text-lg font-bold text-gray-800 my-4 text-center'>
-						Write Introduction Page
-					</h1>
-					<form
-						className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8 text-gray-700'
-						onSubmit={handleSubmit(onSubmit)}
-					>
-						{/* ユーザ名入力フィールド */}
-						<div className='mt-4'>
-							<label className='block text-sm font-bold mb-2' htmlFor='name'>
-								ユーザ名
-							</label>
-							<input
-								className={`border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.name ? 'border-red-500' : 'border-grey-100'}`}
-								id='name'
-								{...register('name', {
-									required: 'ユーザ名は必須です',
-									maxLength: {
-										value: 20,
-										message: '最大20文字です',
-									},
-								})}
-								placeholder='君の名前は？'
-							/>
-							{errors.name && (
-								<p className='absolute text-red-500 text-xs italic'>
-									{errors.name.message}
-								</p>
-							)}
-						</div>
-
-						{/* 自己紹介コメント入力フィールド */}
-						<div className='mt-8'>
-							<label className='block text-sm font-bold mb-2' htmlFor='introduction'>
-								自己紹介コメント
-							</label>
-							<textarea
-								className={`shadow appearance-none border rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.name ? 'border-red-500' : 'border-grey-100'}`}
-								id='introduction'
-								{...register('introduction', {
-									required: '自己紹介コメントは必須です',
-									maxLength: {
-										value: 200,
-										message: '最大200文字です',
-									},
-								})}
-								placeholder='自己紹介スペース'
-							/>
-							{errors.introduction && (
-								<p className='absolute text-red-500 text-xs italic'>
-									{errors.introduction.message}
-								</p>
-							)}
-						</div>
-
-						{/* GithubURL入力フィールド */}
-						<div className='mt-8'>
-							<label className='block text-sm font-bold mb-2' htmlFor='githubUrl'>
-								GitHub URL
-							</label>
-							<input
-								className={`border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.githubUrl ? 'border-red-500' : 'border-grey-100'}`}
-								id='githubUrl'
-								{...register('githubUrl', {
-									pattern: {
-										value: /^(https?:\/\/)?(www\.)?github\.com(\/[^\s]*)?$/i,
-										message:
-											'GitHubのURLを入力してください (例: https://github.com/username)',
-									},
-								})}
-								placeholder='https://github.com/username'
-							/>
-							{errors.githubUrl && (
-								<p className='absolute text-red-500 text-xs italic'>
-									{errors.githubUrl.message}
-								</p>
-							)}
-						</div>
-
-						{/* X URL入力フィールド */}
-						<div className='mt-8'>
-							<label className='block text-sm font-bold mb-2' htmlFor='XUrl'>
-								X URL
-							</label>
-							<input
-								className={`border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.XUrl ? 'border-red-500' : 'border-grey-100'}`}
-								id='XUrl'
-								{...register('XUrl', {
-									pattern: {
-										value: /^(https?:\/\/)?(www\.)?x\.com(\/[^\s]*)?$/i,
-										message:
-											'XのURLを入力してください (例: https://x.com/username)',
-									},
-								})}
-								placeholder='https://x.com/username'
-							/>
-							{errors.XUrl && (
-								<p className='absolute text-red-500 text-xs italic'>
-									{errors.XUrl.message}
-								</p>
-							)}
-						</div>
-
-						{/* イメージ画像選択フィールド */}
-						<div className='mt-8'>
-							<label
-								className={`block text-sm font-bold mb-2 ${errors.image ? 'text-red-500' : 'text-grey-700'}`}
-								htmlFor='image'
-							>
-								画像を選択してください
-							</label>
-							<label>
+				{success && (
+					<div className='w-full max-w-xl'>
+						<h1 className='text-lg font-bold text-gray-800 my-4 text-center'>
+							Write Introduction Page
+						</h1>
+						<form
+							className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8 text-gray-700'
+							onSubmit={handleSubmit(onSubmit)}
+						>
+							{/* ユーザ名入力フィールド */}
+							<div className='mt-4'>
+								<label className='block text-sm font-bold mb-2' htmlFor='name'>
+									ユーザ名
+								</label>
 								<input
-									type='radio'
-									value='0'
-									{...register('image', { required: '画像を選択してください' })}
+									className={`border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.name ? 'border-red-500' : 'border-grey-100'}`}
+									id='name'
+									{...register('name', {
+										required: 'ユーザ名は必須です',
+										maxLength: {
+											value: 20,
+											message: '最大20文字です',
+										},
+									})}
+									placeholder='君の名前は？'
 								/>
-								画像1
-								<DispImage src='/images/robot_and_hogeta.jpeg' alt='sample-alt' />
-							</label>
-							<br />
+								{errors.name && (
+									<p className='absolute text-red-500 text-xs italic'>
+										{errors.name.message}
+									</p>
+								)}
+							</div>
 
-							<label>
-								<input type='radio' value='1' {...register('image')} />
-								画像2
-								<DispImage src='/images/robot_and_hogeta.jpeg' alt='sample-alt' />
-							</label>
-							<br />
+							{/* 自己紹介コメント入力フィールド */}
+							<div className='mt-8'>
+								<label className='block text-sm font-bold mb-2' htmlFor='introduction'>
+									自己紹介コメント
+								</label>
+								<textarea
+									className={`shadow appearance-none border rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.name ? 'border-red-500' : 'border-grey-100'}`}
+									id='introduction'
+									{...register('introduction', {
+										required: '自己紹介コメントは必須です',
+										maxLength: {
+											value: 200,
+											message: '最大200文字です',
+										},
+									})}
+									placeholder='自己紹介スペース'
+								/>
+								{errors.introduction && (
+									<p className='absolute text-red-500 text-xs italic'>
+										{errors.introduction.message}
+									</p>
+								)}
+							</div>
 
-							<label>
-								<input type='radio' value='2' {...register('image')} />
-								画像3
-								<DispImage src='/images/robot_and_hogeta.jpeg' alt='sample-alt' />
-							</label>
-							<br />
+							{/* GithubURL入力フィールド */}
+							<div className='mt-8'>
+								<label className='block text-sm font-bold mb-2' htmlFor='githubUrl'>
+									GitHub URL
+								</label>
+								<input
+									className={`border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.githubUrl ? 'border-red-500' : 'border-grey-100'}`}
+									id='githubUrl'
+									{...register('githubUrl', {
+										pattern: {
+											value: /^(https?:\/\/)?(www\.)?github\.com(\/[^\s]*)?$/i,
+											message:
+												'GitHubのURLを入力してください (例: https://github.com/username)',
+										},
+									})}
+									placeholder='https://github.com/username'
+								/>
+								{errors.githubUrl && (
+									<p className='absolute text-red-500 text-xs italic'>
+										{errors.githubUrl.message}
+									</p>
+								)}
+							</div>
 
-							<label>
-								<input type='radio' value='3' {...register('image')} />
-								画像4
-								<DispImage src='/images/robot_and_hogeta.jpeg' alt='sample-alt' />
-							</label>
-							<br />
-						</div>
+							{/* X URL入力フィールド */}
+							<div className='mt-8'>
+								<label className='block text-sm font-bold mb-2' htmlFor='XUrl'>
+									X URL
+								</label>
+								<input
+									className={`border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.XUrl ? 'border-red-500' : 'border-grey-100'}`}
+									id='XUrl'
+									{...register('XUrl', {
+										pattern: {
+											value: /^(https?:\/\/)?(www\.)?x\.com(\/[^\s]*)?$/i,
+											message:
+												'XのURLを入力してください (例: https://x.com/username)',
+										},
+									})}
+									placeholder='https://x.com/username'
+								/>
+								{errors.XUrl && (
+									<p className='absolute text-red-500 text-xs italic'>
+										{errors.XUrl.message}
+									</p>
+								)}
+							</div>
 
-						{/* 送信ボタン */}
-						<div className='flex justify-center'>
-							<button
-								className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:ring-2 hover:ring-offset-2 hover:ring-blue-600 mt-8'
-								type='submit'
-							>
-								登録する！！
-							</button>
-						</div>
-					</form>
-				</div>
+							{/* イメージ画像選択フィールド */}
+							<div className='mt-8'>
+								<label
+									className={`block text-sm font-bold mb-2 ${errors.image ? 'text-red-500' : 'text-grey-700'}`}
+									htmlFor='image'
+								>
+									画像を選択してください
+								</label>
+								<label>
+									<input
+										type='radio'
+										value='0'
+										{...register('image', { required: '画像を選択してください' })}
+									/>
+									画像1
+									<DispImage src='/images/robot_and_hogeta.jpeg' alt='sample-alt' />
+								</label>
+								<br />
+
+								<label>
+									<input type='radio' value='1' {...register('image')} />
+									画像2
+									<DispImage src='/images/robot_and_hogeta.jpeg' alt='sample-alt' />
+								</label>
+								<br />
+
+								<label>
+									<input type='radio' value='2' {...register('image')} />
+									画像3
+									<DispImage src='/images/robot_and_hogeta.jpeg' alt='sample-alt' />
+								</label>
+								<br />
+
+								<label>
+									<input type='radio' value='3' {...register('image')} />
+									画像4
+									<DispImage src='/images/robot_and_hogeta.jpeg' alt='sample-alt' />
+								</label>
+								<br />
+							</div>
+
+							{/* 送信ボタン */}
+							<div className='flex justify-center'>
+								<button
+									className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:ring-2 hover:ring-offset-2 hover:ring-blue-600 mt-8'
+									type='submit'
+								>
+									登録する！！
+								</button>
+							</div>
+						</form>
+					</div>
+				)}
 			</div>
 		</>
 	);
--- src/pages/FormPages/WriteProductsPage.tsx
+++ src/pages/FormPages/WriteProductsPage.tsx
@@ -61,123 +61,124 @@ export const WriteProductsPage: React.FC = () => {
 
 				{/* ローディング中の処理 */}
 				{loading && <p>Loading...</p>}
+				{success && (
+					<div className='w-full max-w-xl'>
+						<h1 className='text-lg font-bold text-gray-800 my-4 text-center'>
+							Write Products Page
+						</h1>
+						<form
+							className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8 text-gray-700'
+							onSubmit={handleSubmit(onSubmit)}
+						>
+							{/* ユーザ名入力フィールド */}
+							<div className='mt-4'>
+								<label className='block text-sm font-bold mb-2' htmlFor='name'>
+									ユーザ名
+								</label>
+								<input
+									className={`border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.name ? 'border-red-500' : 'border-grey-100'}`}
+									id='name'
+									{...register('name', {
+										required: 'ユーザ名は必須です',
+										maxLength: {
+											value: 20,
+											message: '最大20文字です',
+										},
+									})}
+									placeholder='ユーザ名を入力してけろ'
+								/>
+								{errors.name && (
+									<p className='absolute text-red-500 text-xs italic'>
+										{errors.name.message}
+									</p>
+								)}
+							</div>
 
-				<div className='w-full max-w-xl'>
-					<h1 className='text-lg font-bold text-gray-800 my-4 text-center'>
-						Write Products Page
-					</h1>
-					<form
-						className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8 text-gray-700'
-						onSubmit={handleSubmit(onSubmit)}
-					>
-						{/* ユーザ名入力フィールド */}
-						<div className='mt-4'>
-							<label className='block text-sm font-bold mb-2' htmlFor='name'>
-								ユーザ名
-							</label>
-							<input
-								className={`border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.name ? 'border-red-500' : 'border-grey-100'}`}
-								id='name'
-								{...register('name', {
-									required: 'ユーザ名は必須です',
-									maxLength: {
-										value: 20,
-										message: '最大20文字です',
-									},
-								})}
-								placeholder='ユーザ名を入力してけろ'
-							/>
-							{errors.name && (
-								<p className='absolute text-red-500 text-xs italic'>
-									{errors.name.message}
-								</p>
-							)}
-						</div>
+							{/* タイトル入力フィールド */}
+							<div className='mt-8'>
+								<label className='block text-sm font-bold mb-2' htmlFor='title'>
+									タイトル
+								</label>
+								<input
+									className={`border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.title ? 'border-red-500' : 'border-grey-100'}`}
+									id='title'
+									{...register('title', {
+										required: 'タイトルは必須です',
+										maxLength: {
+											value: 50,
+											message: '最大50文字です',
+										},
+									})}
+									placeholder='タイトルを入力してください'
+								/>
+								{errors.title && (
+									<p className='absolute text-red-500 text-xs italic'>
+										{errors.title.message}
+									</p>
+								)}
+							</div>
 
-						{/* タイトル入力フィールド */}
-						<div className='mt-8'>
-							<label className='block text-sm font-bold mb-2' htmlFor='title'>
-								タイトル
-							</label>
-							<input
-								className={`border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.title ? 'border-red-500' : 'border-grey-100'}`}
-								id='title'
-								{...register('title', {
-									required: 'タイトルは必須です',
-									maxLength: {
-										value: 50,
-										message: '最大50文字です',
-									},
-								})}
-								placeholder='タイトルを入力してください'
-							/>
-							{errors.title && (
-								<p className='absolute text-red-500 text-xs italic'>
-									{errors.title.message}
-								</p>
-							)}
-						</div>
+							{/* URL入力フィールド */}
+							<div className='mt-8'>
+								<label className='block text-sm font-bold mb-2' htmlFor='url'>
+									URL
+								</label>
+								<input
+									className={`border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.url ? 'border-red-500' : 'border-grey-100'}`}
+									id='url'
+									{...register('url', {
+										required: 'URLは必須です',
+										pattern: {
+											value: /^(https?:\/\/)?([a-z0-9]+[.])*[a-z0-9]+\.[a-z]+(\/[^\s]*)?$/i,
+											message: '有効なURLを入力してください',
+										},
+									})}
+									placeholder='https://example.com'
+								/>
+								{errors.url && (
+									<p className='absolute text-red-500 text-xs italic'>
+										{errors.url.message}
+									</p>
+								)}
+							</div>
 
-						{/* URL入力フィールド */}
-						<div className='mt-8'>
-							<label className='block text-sm font-bold mb-2' htmlFor='url'>
-								URL
-							</label>
-							<input
-								className={`border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline ${errors.url ? 'border-red-500' : 'border-grey-100'}`}
-								id='url'
-								{...register('url', {
-									required: 'URLは必須です',
-									pattern: {
-										value: /^(https?:\/\/)?([a-z0-9]+[.])*[a-z0-9]+\.[a-z]+(\/[^\s]*)?$/i,
-										message: '有効なURLを入力してください',
-									},
-								})}
-								placeholder='https://example.com'
-							/>
-							{errors.url && (
-								<p className='absolute text-red-500 text-xs italic'>
-									{errors.url.message}
-								</p>
-							)}
-						</div>
+							{/* コメント入力フィールド */}
+							<div className='mt-8'>
+								<label className='block text-sm font-bold mb-2' htmlFor='comment'>
+									コメント
+								</label>
+								<textarea
+									className='border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline'
+									id='comment'
+									{...register('comment', {
+										maxLength: {
+											value: 200,
+											message: '最大200文字です',
+										},
+									})}
+									placeholder='成果物についての説明やコメントを入力してください'
+								/>
+								{errors.comment && (
+									<p className='absolute text-red-500 text-xs italic'>
+										{errors.comment.message}
+									</p>
+								)}
+							</div>
 
-						{/* コメント入力フィールド */}
-						<div className='mt-8'>
-							<label className='block text-sm font-bold mb-2' htmlFor='comment'>
-								コメント
-							</label>
-							<textarea
-								className='border shadow appearance-none rounded w-full py-2 px-3 mb-2 leading-tight focus:outline-none focus:shadow-outline'
-								id='comment'
-								{...register('comment', {
-									maxLength: {
-										value: 200,
-										message: '最大200文字です',
-									},
-								})}
-								placeholder='成果物についての説明やコメントを入力してください'
-							/>
-							{errors.comment && (
-								<p className='absolute text-red-500 text-xs italic'>
-									{errors.comment.message}
-								</p>
-							)}
-						</div>
-
-						{/* 送信ボタン */}
-						<div className='flex justify-center'>
-							<button
-								className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:ring-2 hover:ring-offset-2 hover:ring-blue-600 mt-12'
-								onMouseEnter={() => setText('投稿する！！')}
-								onMouseLeave={() => setText('投稿する？')}
-								type='submit'
-							>
-								{text}
-							</button>
-						</div>
-					</form>
-				</div>
+							{/* 送信ボタン */}
+							<div className='flex justify-center'>
+								<button
+									className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:ring-2 hover:ring-offset-2 hover:ring-blue-600 mt-12'
+									onMouseEnter={() => setText('投稿する！！')}
+									onMouseLeave={() => setText('投稿する？')}
+									type='submit'
+								>
+									{text}
+								</button>
+							</div>
+						</form>
+					</div>
+				)}
 			</div>
 		</>
 	);
