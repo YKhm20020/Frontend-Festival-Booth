c37f190678bdd4e33238e7a5f66abf844fdab8e5
takahashi <tkhs.tmhr39@gmail.com>
2024-09-14 21:44:34 +0900

    fix: name欄の表記をメールアドレスからユーザ名に修正

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -70,12 +70,12 @@ export const AuthPage: React.FC = () => {
 								htmlFor='email'
 								className='block text-sm font-medium text-gray-700'
 							>
-								メールアドレス
+								ユーザ名
 							</label>
 							<input
 								id='email'
 								// type='email'
-								{...register('email', { required: 'メールアドレスは必須です' })}
+								{...register('email', { required: 'ユーザ名は必須です' })}
 								className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 transition duration-150 ease-in-out'
 							/>
 							{errors.email && (
