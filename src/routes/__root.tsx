7d0f301d794aaf232701986dee0a6ecc5d4be73b
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-15 14:35:44 +0900

    update: ビルドを通すため、不要なimport文を削除

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+// import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
 	component: () => (
