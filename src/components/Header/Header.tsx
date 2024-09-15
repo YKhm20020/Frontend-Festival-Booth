381b873c2d13c94d1e45750287f0ee00ace3b2bd
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-15 13:43:14 +0900

    update: 余計な余白の調整、左上のロゴからホームに遷移できるようにし、右上からHomeを削除

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,15 @@
 import type React from 'react';
+import { Link } from '@tanstack/react-router';
 import { NavLink } from './_components/NavLink';
 
 export const Header: React.FC = () => {
 	return (
-		<header className='bg-blue-400 flex w-screen h-auto my-auto py-4'>
-			<div className='container items-center my-auto ml-4 text-lg font-bold'>
+		<header className='bg-blue-400 flex w-full h-auto my-auto py-4'>
+			<Link to='/' className='container items-center my-auto ml-4 text-lg font-bold'>
 				Online Festival Booth
-			</div>
+			</Link>
 			<div className='container mx-auto flex flex-col md:flex-row items-center'>
 				<nav className='ml-auto flex flex-wrap items-center justify-center '>
-					<NavLink toLink='/'>Home</NavLink>
 					<NavLink toLink='/introduction-list'>Read Intro</NavLink>
 					<NavLink toLink='/write-introduction'>Write Intro</NavLink>
 					<NavLink toLink='/products-list'>Read Prods</NavLink>
