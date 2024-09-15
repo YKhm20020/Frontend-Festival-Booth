84cb6d2736ff48a1eafc342512e892eaa5bd082a
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-15 13:31:43 +0900

    update: バックエンドのicon_numとの統合を図るため、srcの文字列を辞書のvalueから引っ張ってくるよう修正

--- src/pages/MainPages/IntroductionListPage.tsx
+++ src/pages/MainPages/IntroductionListPage.tsx
@@ -1,5 +1,6 @@
 import type React from 'react';
 import { useState, useMemo } from 'react';
+import { useLocation } from '@tanstack/react-router';
 import { useGetProfile } from '../../hooks/useGetProfile';
 import { useGetProducts } from '../../hooks/useGetProducts';
 import { Header } from '../../components/Header/Header';
@@ -8,38 +9,47 @@ import { Card } from '../../components/Cards/Card';
 export const IntroductionListPage: React.FC = () => {
 	const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-	// 1ページあたりに配置するカードの数
+	const dicImage: { [imageNum: string]: string } = {
+		'0': '/images/robot_and_hogeta.jpeg',
+		'1': '/images/horse.jpg',
+		'2': 'images/otter.png',
+		'3': 'images/scale.jpg',
+	};
+
+	// 1ページあたりに表示するカードの数
 	const cardsPerPage = 8;
 
+	// クエリパラメータから answer を取得
+	const location = useLocation();
+	const searchParams = new URLSearchParams(location.search);
+	const answerFromParams = searchParams.get('answer');
+
+	// useGetProfile と useGetProducts からデータを取得
 	const { data: introduction = [], loading, error } = useGetProfile({});
 	const { data: products } = useGetProducts({});
 
+	// すべてのカード
 	const allCards = useMemo(() => {
 		return Array.isArray(introduction) ? introduction : [];
 	}, [introduction]);
 
-	// タブの合計、小数点以下切り上げ
+	// 現在のページに表示するカード
+	const currentPageCards = useMemo(() => {
+		const cards = allCards.slice(
+			currentIndex * cardsPerPage,
+			(currentIndex + 1) * cardsPerPage,
+		);
+
+		// answer パラメータが存在する場合にその値でフィルタリング
+		if (answerFromParams) {
+			return cards.filter((card) => card.answer === Number.parseInt(answerFromParams));
+		}
+		// パラメータがない場合はすべてのカードを表示
+		return cards;
+	}, [allCards, currentIndex, answerFromParams]);
+
+	// 総スライド数を計算
 	const numOfSlides = Math.ceil(allCards.length / cardsPerPage);
-
-	// 現在のページのカードを取得
-	const currentPageCards = allCards.slice(
-		currentIndex * cardsPerPage,
-		(currentIndex + 1) * cardsPerPage,
-	);
-
-	// カードのモックデータ
-	// const allCards = useMemo(() => {
-	// 	return Array(numOfSlides * cardsPerPage)
-	// 		.fill(null)
-	// 		.map((_, index) => ({
-	// 			src: '/images/robot_and_hogeta.jpeg',
-	// 			alt: 'Sample Alt',
-	// 			title: `Card ${index + 1}`,
-	// 			links: ['link1', 'link2', 'link3'],
-	// 			modalTitle: `Sample Title ${index + 1}`,
-	// 			modalText: `Sample Text for card ${index + 1}`,
-	// 		}));
-	// }, []);
 
 	const prevSlide = (): void => {
 		setCurrentIndex((prevIndex) => (prevIndex - 1 + numOfSlides) % numOfSlides);
@@ -54,27 +64,24 @@ export const IntroductionListPage: React.FC = () => {
 			<Header />
 			<div className='w-full max-w-screen-lg mx-auto p-4 my-8'>
 				<div className='relative'>
-					{/* カードのグリッド */}
 					<div className='grid grid-cols-2 md:grid-cols-4 md:grid-rows-2 lg:gap-x-6 gap-y-8 place-items-center justify-center items-center'>
-						{/* ローディング中またはエラーがある場合の処理 */}
 						{loading && <p>Loading...</p>}
 						{error && <p>Error: {error}</p>}
-						{/* カードコンポーネント（1ページにつき8個） */}
 						{currentPageCards.map((card, index) => {
 							const isMatched =
 								products &&
 								index < products.length &&
-								products[index].user_name === card.name;
-							console.log(products[index]);
-							console.log(currentPageCards.length);
-							console.log(index);
+								card.name === products[index].user_name;
+
+							const imageSrc =
+								dicImage[card.icon_num] || '/images/robot_and_hogeta.jpeg';
 
 							return (
 								<Card
 									key={currentIndex * cardsPerPage + index}
 									userName={card.name}
-									src='/images/robot_and_hogeta.jpeg' // TODO: src を icon_num に対応した画像のパスに変更する
-									alt='Sample Alt' // TODO: alt は icon_num に対応する画像データに合わせて変更
+									src={imageSrc}
+									alt='Sample Alt'
 									title={card.name}
 									links={{
 										github_url: card.github_url,
@@ -84,20 +91,21 @@ export const IntroductionListPage: React.FC = () => {
 									modalText={card.introduction || 'Sample Text'}
 									theOtherModalTitle={isMatched ? products[index].title : ''}
 									theOtherModalText={isMatched ? products[index].description : ''}
-									theOtherModalLinks={isMatched ? {
-										url: products[index].url
-									} : {}}
+									theOtherModalLinks={
+										isMatched
+											? {
+													url: products[index].url,
+												}
+											: {}
+									}
 								/>
 							);
 						})}
 					</div>
 
-					{/* 左右のボタン */}
 					<div className='absolute left-0 lg:-left-12 top-1/2 -translate-y-1/2'>
 						<button
-							id='slider-button-left'
-							className='group p-2 flex justify-center items-center border border-solid border-indigo-600 w-20 h-20 md:w-16 md:h-16 transition-all duration-500 rounded-full
-                            hover:bg-indigo-600 bg-white bg-opacity-50'
+							className='group p-2 flex justify-center items-center border border-solid border-indigo-600 w-20 h-20 md:w-16 md:h-16 transition-all duration-500 rounded-full hover:bg-indigo-600 bg-white bg-opacity-50'
 							onClick={prevSlide}
 						>
 							<svg
@@ -118,9 +126,7 @@ export const IntroductionListPage: React.FC = () => {
 					</div>
 					<div className='absolute right-0 lg:-right-12 top-1/2 -translate-y-1/2'>
 						<button
-							id='slider-button-right'
-							className='group p-2 flex justify-center items-center border border-solid border-indigo-600 w-20 h-20 md:w-16 md:h-16 transition-all duration-500 rounded-full
-                            hover:bg-indigo-600 bg-white bg-opacity-50'
+							className='group p-2 flex justify-center items-center border border-solid border-indigo-600 w-20 h-20 md:w-16 md:h-16 transition-all duration-500 rounded-full hover:bg-indigo-600 bg-white bg-opacity-50'
 							onClick={nextSlide}
 						>
 							<svg
@@ -142,20 +148,18 @@ export const IntroductionListPage: React.FC = () => {
 				</div>
 			</div>
 			<div className='absolute mt-auto left-0 right-0 z-10 mx-2 mb-4 flex list-none justify-center p-0'>
-				<div className='absolute mt-auto left-0 right-0 z-10 mx-2 mb-4 flex list-none justify-center p-0'>
-					{[...Array(numOfSlides)].map((_, index) => (
-						<button
-							key={index}
-							type='button'
-							onClick={() => setCurrentIndex(index)}
-							className={`mx-1 box-content h-1 w-16 flex-initial cursor-pointer border-0 border-y-8 border-solid border-transparent bg-clip-padding p-0 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none
+				{[...Array(numOfSlides)].map((_, index) => (
+					<button
+						key={index}
+						type='button'
+						onClick={() => setCurrentIndex(index)}
+						className={`mx-1 box-content h-1 w-16 flex-initial cursor-pointer border-0 border-y-8 border-solid border-transparent bg-clip-padding p-0 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none
                             ${currentIndex === index ? 'bg-indigo-600 opacity-100' : 'bg-black opacity-50'}
                             `}
-							aria-current={currentIndex === index ? 'true' : 'false'}
-							aria-label={`Tab ${index}`}
-						/>
-					))}
-				</div>
+						aria-current={currentIndex === index ? 'true' : 'false'}
+						aria-label={`Tab ${index}`}
+					/>
+				))}
 			</div>
 		</>
 	);
