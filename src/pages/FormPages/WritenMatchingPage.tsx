b06908f7be966024c25dcf540a589fe27475ba4b
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-09-15 14:44:23 +0900

    update: DBへの存在しないユーザーの値書き込みを防ぐため、マッチングページへのアクセスをログイン必須に変更

--- src/pages/FormPages/WritenMatchingPage.tsx
+++ src/pages/FormPages/WritenMatchingPage.tsx
@@ -1,7 +1,9 @@
 import type React from 'react';
-import { useLocation, useRouter } from '@tanstack/react-router';
+import { useEffect } from 'react';
+import { useLocation, useRouter, useNavigate } from '@tanstack/react-router';
 import { useForm } from 'react-hook-form';
 import type { SubmitHandler } from 'react-hook-form';
+import { useGetLoginStatus } from '../../hooks/useGetLoginStatus';
 import { usePostAnswers } from '../../hooks/usePostAnswers';
 import { Header } from '../../components/Header/Header';
 
@@ -24,11 +26,20 @@ type QuestionsFormData = {
 };
 
 export const WritenMatchingPage: React.FC = () => {
+	const { success, loading } = useGetLoginStatus();
 	const { postAnswers, error } = usePostAnswers();
 
 	const location = useLocation();
 	const searchParams = new URLSearchParams(location.search);
 	const router = useRouter();
+	const navigate = useNavigate();
+
+	// successがfalseのときにauthページにリダイレクトする処理を追加
+	useEffect(() => {
+		if (!loading && !success) {
+			navigate({ to: '/auth' });
+		}
+	}, [loading, success, navigate]);
 
 	const defaultValues: QuestionsFormData = {
 		question0: Number(searchParams.get('question0')) || 0,
@@ -67,51 +78,59 @@ export const WritenMatchingPage: React.FC = () => {
 		<>
 			<Header />
 			<div className='mt-8'>
-				<h1 className='text-2xl font-bold text-center text-red-700'>
-					誰とつながるかな？ マッチング診断
-				</h1>
-				<div className='px-4 max-w-md mx-auto bg-red-100 shadow-lg space-y-4'>
-					<form onSubmit={handleSubmit(onSubmit)}>
-						{Questions.map((question, qIndex) => (
-							<div key={qIndex} className='border-t border-red-300 pt-4 px-4'>
-								<h2 className='text-xl font-semibold text-red-600'>{question}</h2>
-								<div className='mt-2'>
-									{Options[qIndex].map((option, oIndex) => (
-										<label key={oIndex} className='block text-lg'>
-											{oIndex === 0 ? (
-												<input
-													type='radio'
-													value={oIndex}
-													{...register(
-														`question${qIndex}` as `question${0 | 1 | 2}`,
+				{/* ローディング中の処理 */}
+				{loading && <p>Loading...</p>}
+				{success && (
+					<>
+						<h1 className='text-2xl font-bold text-center text-red-700'>
+							誰とつながるかな？ マッチング診断
+						</h1>
+						<div className='px-4 max-w-md mx-auto bg-red-100 shadow-lg space-y-4'>
+							<form onSubmit={handleSubmit(onSubmit)}>
+								{Questions.map((question, qIndex) => (
+									<div key={qIndex} className='border-t border-red-300 pt-4 px-4'>
+										<h2 className='text-xl font-semibold text-red-600'>
+											{question}
+										</h2>
+										<div className='mt-2'>
+											{Options[qIndex].map((option, oIndex) => (
+												<label key={oIndex} className='block text-lg'>
+													{oIndex === 0 ? (
+														<input
+															type='radio'
+															value={oIndex}
+															{...register(
+																`question${qIndex}` as `question${0 | 1 | 2}`,
+															)}
+															className='mr-2'
+														/>
+													) : (
+														<input
+															type='radio'
+															value={oIndex}
+															required
+															{...register(
+																`question${qIndex}` as `question${0 | 1 | 2}`,
+															)}
+															className='mr-2'
+														/>
 													)}
-													className='mr-2'
-												/>
-											) : (
-												<input
-													type='radio'
-													value={oIndex}
-													required
-													{...register(
-														`question${qIndex}` as `question${0 | 1 | 2}`,
-													)}
-													className='mr-2'
-												/>
-											)}
-											{option}
-										</label>
-									))}
-								</div>
-							</div>
-						))}
-						<button
-							type='submit'
-							className='w-full mt-4 bg-red-500 text-white py-2 rounded hover:bg-red-600'
-						>
-							登録する！！
-						</button>
-					</form>
-				</div>
+													{option}
+												</label>
+											))}
+										</div>
+									</div>
+								))}
+								<button
+									type='submit'
+									className='w-full mt-4 bg-red-500 text-white py-2 rounded hover:bg-red-600'
+								>
+									登録する！！
+								</button>
+							</form>
+						</div>
+					</>
+				)}
 			</div>
 		</>
 	);
